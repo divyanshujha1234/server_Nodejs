b6daabe16e9ad4264543050b3daaf1a624ec43e8
divyanshu jha <divyanshu3636@gmail.com>
2025-01-23 01:33:28 +0530

    start of expressjs

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const http= require("http");
 const fs=require("fs");
 const url=require("url"); //in dependencies (npm install url)
-const myServer= http.createServer((req,res)=> {  //request and response
+
+function handler(req,res)
+{
     if(req.url==="/favicon.ico")
         return res.end();
     const log= `${Date.now()}: ${req.method} ${req.url} New request Recieved\n`;
     const myUrl= url.parse(req.url,true); //parse query string 
-    console.log(myUrl);
+    //console.log(myUrl);
     fs.appendFile('log.txt', log, (err,data)=>{
        const username=myUrl.query.myname;
        switch(myUrl.pathname)
@@ -18,6 +20,14 @@ const myServer= http.createServer((req,res)=> {  //request and response
           case "/about":
             res.end(`hi ${username}`);
             break;
+          case "/signup":
+            if(req.method === "GET")
+                res.end("This is the sign up form");
+            else if(req.method==="POST")
+            {
+                //DB quries
+                res.end("Success");
+            }
           default:
             res.end("default case");
 
@@ -26,7 +36,8 @@ const myServer= http.createServer((req,res)=> {  //request and response
         
     });
 
-});
+}
+const myServer= http.createServer(handler)  //request and response
 
 myServer.listen(8000, () => console.log("Server started!"));  //port number .
 
@@ -36,4 +47,8 @@ myServer.listen(8000, () => console.log("Server started!"));  //port number .
 // PUT- TO PUTTING SOME DATA LIKE PHOTO OR PUTTING RESUME 
 // PATCH- TO CHANGE ANY DATA 
 // DELETE- TO DELETE
+
+
+// Now, mainting the code for http request and respnse is getting complicated. 
+// So to counter this we have a nodejs framework EXPRESS.JS (npm install express)
  
