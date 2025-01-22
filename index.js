53de4f25b0e93bd1d02af8fd12ad6ca8d5501f88
divyanshu jha <divyanshu3636@gmail.com>
2025-01-23 00:32:58 +0530

    about URL

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // ctrl+c for killing the serever
 const http= require("http");
 const fs=require("fs");
+const url=require("url"); //in dependencies (npm install url)
 const myServer= http.createServer((req,res)=> {  //request and response
-    const log= `${Date.now()}: New rquest Recieved\n`;
-    
+    if(req.url==="/favicon.ico")
+        return res.end();
+    const log= `${Date.now()}: ${req.url} New rquest Recieved\n`;
+    const myUrl= url.parse(req.url,true); //parse query string 
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (err,data)=>{
+        
+       switch(myUrl.pathname)
+       {
+          case "/":
+            res.end("homepage");
+            break;
+          case "/about":
+            res.end("about page");
+            break;
+          default:
+            res.end("default case");
 
-        res.end("hello from server");
+       }
+
+        
     });
 
 });
