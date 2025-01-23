286007a357764a889122db903fac608fb26c124a
divyanshu jha <divyanshu3636@gmail.com>
2025-01-23 21:12:56 +0530

    url pacakge removed and no express wuhuu

--- index.js
+++ index.js
@@ -1,45 +1,60 @@
 // ctrl+c for killing the serever
-const http= require("http");
-const fs=require("fs");
-const url=require("url"); //in dependencies (npm install url)
+ const http= require("http");
+// const fs=require("fs");
+// const url=require("url"); //in dependencies (npm install url)
+const express= require("express");
 
-function handler(req,res)
-{
-    if(req.url==="/favicon.ico")
-        return res.end();
-    const log= `${Date.now()}: ${req.method} ${req.url} New request Recieved\n`;
-    const myUrl= url.parse(req.url,true); //parse query string 
-    //console.log(myUrl);
-    fs.appendFile('log.txt', log, (err,data)=>{
-       const username=myUrl.query.myname;
-       switch(myUrl.pathname)
-       {
-          case "/":
-            res.end("homepage");
-            break;
-          case "/about":
-            res.end(`hi ${username}`);
-            break;
-          case "/signup":
-            if(req.method === "GET")
-                res.end("This is the sign up form");
-            else if(req.method==="POST")
-            {
-                //DB quries
-                res.end("Success");
-            }
-          default:
-            res.end("default case");
+const app= express();  // this app is like a handler function (we have all the methods) 
 
-       }
+app.get("/", (req,res)=> {
+     return res.send("hello from homepage");
+});
+// app.HTTP_METHOD(path,handler);
+   // app is instance of express
+   //path on the server
+   //handeler is executed when route is matched 
+app.get("/about", (req,res)=>{
+   return res.send("hello from about page");
+});
+// function handler(req,res)
+// {
+//     if(req.url==="/favicon.ico")
+//         return res.end();
+//     const log= `${Date.now()}: ${req.method} ${req.url} New request Recieved\n`;
+//     const myUrl= url.parse(req.url,true); //parse query string 
+//     //console.log(myUrl);
+//     fs.appendFile('log.txt', log, (err,data)=>{
+//        const username=myUrl.query.myname;
+//        switch(myUrl.pathname)
+//        {
+//           case "/":
+//             res.end("homepage");
+//             break;
+//           case "/about":
+//             res.end(`hi ${username}`);
+//             break;
+//           case "/signup":
+//             if(req.method === "GET")
+//                 res.end("This is the sign up form");
+//             else if(req.method==="POST")
+//             {
+//                 //DB quries
+//                 res.end("Success");
+//             }
+//           default:
+//             res.end("default case");
+
+//        }
 
         
-    });
+//     });
 
-}
-const myServer= http.createServer(handler)  //request and response
+// }
+// const myServer= http.createServer(app);  //request and response
 
-myServer.listen(8000, () => console.log("Server started!"));  //port number .
+// myServer.listen(8000, () => console.log("Server started!"));  //port number .
+
+app.listen(8000,()=> console.log("Server Started!"));
 
 // ***HTTP METHODS-***
 // GET- WHEN YOU WANT TO GET SOME DATA FROM THE SERVER 
@@ -51,4 +66,5 @@ myServer.listen(8000, () => console.log("Server started!"));  //port number .
 
 // Now, mainting the code for http request and respnse is getting complicated. 
 // So to counter this we have a nodejs framework EXPRESS.JS (npm install express)
+// we don't evem need to import the http to run the server in express
  
